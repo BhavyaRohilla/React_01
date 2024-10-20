f5d6c27f5b5efbe5d99a31583ad4664b8794273d
Bhavya <officialbhavyarohilla@gmail.com>
2024-10-20 21:49:00 +0530

    d01

---
+++ App.js
@@ -0,0 +1,24 @@
+/*
+ReactElement(Object) => HTML(Browser Understands)
+*/
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "I'm an h2 tag"),
+  ])
+);
+
+console.log(parent); // object
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading", xyz: "abc" },
+  "Hello World From React!!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
